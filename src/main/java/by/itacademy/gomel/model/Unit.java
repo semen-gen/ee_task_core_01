6ed2e76ab68020cb0472c255b69bceb36736082b
Ivan <semen_gen@tut.by>
2020-07-24 20:29:31 +0300

    Сделал набор единиц измерений

---
+++ src/main/java/by/itacademy/gomel/model/Unit.java
@@ -0,0 +1,24 @@
+package by.itacademy.gomel.model;
+
+import java.util.Arrays;
+
+public enum Unit {
+    MS("ms"),
+    KMH("kmh"),
+    MPH("mph"),
+    KN("kn");
+
+    private final String VAL;
+
+    Unit(String unit) {
+        VAL = unit;
+    }
+
+    public String getVAL() {
+        return VAL;
+    }
+
+    public static Unit getByValue(String unit) {
+        return Arrays.stream(values()).filter(e -> e.getVAL().equals(unit)).findFirst().get();
+    }
+}
