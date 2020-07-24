82b6f7474497127877c6386f0d04e2db887a9422
Ivan <semen_gen@tut.by>
2020-07-24 20:28:00 +0300

    repo init

---
+++ src/main/java/by/itacademy/gomel/App.java
@@ -0,0 +1,9 @@
+package by.itacademy.gomel;
+
+import by.itacademy.gomel.model.Unit;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Unit.getByValue("ms"));
+    }
+}
