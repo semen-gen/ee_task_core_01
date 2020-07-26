8835ea06be2a7c72afd5cba9d09347c5ff8290cf
Ivan <semen_gen@tut.by>
2020-07-27 01:38:42 +0300

    Добавил класс сервиса, перенёс туда функционал, сделал имя файла через параметр

--- src/main/java/by/itacademy/gomel/App.java
+++ src/main/java/by/itacademy/gomel/App.java
@@ -2,69 +2,15 @@ package by.itacademy.gomel;
 
 import by.itacademy.gomel.comporators.SpeedASC;
 import by.itacademy.gomel.comporators.SpeedUnitAndValueDESC;
-import by.itacademy.gomel.model.Speed;
-
-import java.io.*;
-import java.util.*;
+import by.itacademy.gomel.servicese.Service;
 
 public class App {
-    private static Scanner scanner = new Scanner(System.in);
-    private static List<Speed> collection = new ArrayList<>();
 
-    public static void main(String[] args) {
-        getFileCollection();
-        printCollectionInMS();
-        printSortCollection(new SpeedASC());
-        printIsInCollectionValue(2.0, "kmh");
-        printSortCollection(new SpeedUnitAndValueDESC());
-    }
-
-    private static void printIsInCollectionValue(double value, String unit) {
-        System.out.println(collection.stream().anyMatch(s -> s.getValue() == value && s.getUnit().equals(unit)) ? "yes" : "no");
-        System.out.println();
-    }
-
-    private static void printSortCollection(Comparator<Speed> comparator) {
-        collection.sort(comparator);
-        printCollection();
-    }
-
-    private static void printCollection() {
-        for (Speed item : collection) {
-            System.out.println(item);
-        }
-        System.out.println();
-    }
-
-    private static void printCollectionInMS() {
-        for (Speed item : collection) {
-            System.out.println(item + " = " + item.InMS());
-        }
-        System.out.println();
-    }
-
-    private static String getFileName() {
-        String name = scanner.nextLine();
-        scanner.close();
-        return name;
-    }
-
-    private static File getFile() {
-        return new File("src/main/resources/files/" + getFileName());
-    }
-
-    private static List<Speed> getFileCollection() {
-        File file = getFile();
-        try (FileReader fr = new FileReader(file)) {
-            BufferedReader reader = new BufferedReader(fr);
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] arr = line.split("\\s+", 2);
-                collection.add(new Speed(new Double(arr[0]), arr[1]));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return collection;
-    }
+  public static void main(String[] args) {
+    Service service = new Service(args[0]);
+    service.printCollectionInMS();
+    service.printSortCollection(new SpeedASC());
+    service.printIsInCollectionValue(2.0, "kmh");
+    service.printSortCollection(new SpeedUnitAndValueDESC());
+  }
 }
---
+++ src/main/java/by/itacademy/gomel/servicese/Service.java
@@ -0,0 +1,59 @@
+package by.itacademy.gomel.servicese;
+
+import by.itacademy.gomel.model.Speed;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class Service {
+
+  protected File file;
+  private List<Speed> collection;
+
+  public Service(String fileName) {
+    file = new File("src/main/resources/files/" + fileName);
+    collection = getFileCollection();
+  }
+
+  public void printIsInCollectionValue(double value, String unit) {
+    System.out.println(
+        collection.stream()
+            .anyMatch(s -> s.getValue() == value && s.getUnit().equals(unit)) ? "yes" : "no");
+    System.out.println();
+  }
+
+  public void printSortCollection(Comparator<Speed> comparator) {
+    collection.sort(comparator);
+    printCollection();
+  }
+
+  private void printCollection() {
+    for (Speed item : collection) {
+      System.out.println(item);
+    }
+    System.out.println();
+  }
+
+  public void printCollectionInMS() {
+    for (Speed item : collection) {
+      System.out.println(item + " = " + item.InMS());
+    }
+    System.out.println();
+  }
+
+  public List<Speed> getFileCollection() {
+    List<Speed> collection = new ArrayList<>();
+    try {
+      Files.lines(file.toPath())
+          .map(l -> l.split("\\s+", 2))
+          .forEach(arr -> collection.add(new Speed(new Double(arr[0]), arr[1])));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return collection;
+  }
+}
